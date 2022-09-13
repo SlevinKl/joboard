920892eee0d36e6e789a1d4f074866776eea6ee6
Rayane Moula <moula.rayane@gmail.com>
2022-09-13 17:37:10 +0200

    new applciation model

---
+++ db/migrate/20220913153218_create_applications.rb
@@ -0,0 +1,14 @@
+class CreateApplications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :applications do |t|
+      t.string :title
+      t.string :category
+      t.string :location
+      t.string :experience
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
