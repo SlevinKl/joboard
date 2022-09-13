684699ac4db148dfe6d0367db145522d701014fe
Rayane Moula <moula.rayane@gmail.com>
2022-09-13 18:03:26 +0200

    controller application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'applications/index'
+  get 'applications/new'
+  get 'applications/create'
+  get 'applications/edit'
+  get 'applications/update'
+  get 'applications/destroy'
   devise_for :users
   root to: "pages#home"
+
+  resources :applications
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
